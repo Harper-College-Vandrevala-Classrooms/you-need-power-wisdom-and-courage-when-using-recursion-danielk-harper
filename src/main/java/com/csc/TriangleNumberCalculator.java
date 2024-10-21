49bc1d804b7eb5c87d9eca7dd337d5f3111b1823
danielk-harper <d_kaeseberg1@mail.harpercollege.edu>
2024-10-20 23:13:12 -0500

    Project

--- src/main/java/com/csc/TriangleNumberCalculator.java
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -1,5 +1,53 @@
 package com.csc;
 
 public class TriangleNumberCalculator {
-  // Your code goes here!
+  private int[] triangle_cache;
+  
+  public TriangleNumberCalculator() {
+    triangle_cache = new int[1000];
+    for(int i = 0; i < 1000; i++) {
+      triangle_cache[i] = 0;
+    }
+  }
+  
+  public static void main(String[] args) {
+    TriangleNumberCalculator t = new TriangleNumberCalculator();
+    
+    for(int i = 1000; i < 33000; i++) {
+
+      System.out.print(i);
+      System.out.print(" ");
+      
+      t.subtract(i, 1);
+    }
+  }
+  
+  private boolean has_value_cached(int n) {
+    return triangle_cache[n - 1] > 0;
+  }
+  
+  public int value(int n) {
+    if (n <= 1) return 1;
+    
+    if (n <= 1000) {
+      if (has_value_cached(n)) {
+        return triangle_cache[n - 1];
+      } else {
+        int actual_num = n + this.value(n - 1);
+        triangle_cache[n - 1] = actual_num;
+        return actual_num;
+      }
+      
+    } else {
+      return n + this.value(n - 1);
+    }
+  }
+  
+  public int add(int n, int m) {
+    return this.value(n) + this.value(m);
+  }
+  
+  public int subtract(int n, int m) {
+    return this.value(n) - this.value(m);
+  }
 }
--- src/test/java/com/csc/TestTriangleNumberCalculator.java
+++ src/test/java/com/csc/TestTriangleNumberCalculator.java
@@ -1,6 +1,9 @@
 package com.csc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestTriangleNumberCalculator {
 
@@ -9,6 +12,27 @@ public class TestTriangleNumberCalculator {
   @BeforeEach
   void setUp() {
     calculator = new TriangleNumberCalculator();
+  }
+  
+  @Test
+  void testValue() {
+    assertEquals(1, calculator.value(1));
+    assertEquals(3, calculator.value(2));
+    assertEquals(10, calculator.value(4));
+  }
+  
+  @Test
+  void testAdd() {
+    assertEquals(2, calculator.add(1, 1));
+    assertEquals(9, calculator.add(2, 3));
+    assertEquals(13, calculator.add(4, 2));
+  }
+  
+  @Test
+  void testSubtract() {
+    assertEquals(0, calculator.subtract(1, 1));
+    assertEquals(-3, calculator.subtract(2, 3));
+    assertEquals(7, calculator.subtract(4, 2));
   }
 
   // Your tests go here
